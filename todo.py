97c86ef89efe25ce7f957d23d26cbfdea1e7ddbc
Mabe <bokaj-eu@web.de>
2025-09-25 20:55:18 +0200

    V 2.0 SavedToDos.json added Saving content when closing

--- todo.py
+++ todo.py
@@ -1,16 +1,39 @@
+import json
+
 todos = []
 
-for _ in range(10):
+
+# Speichern in einer .txt-Datei
+def saveList(values):
+
+    with open("SavedToDos.json", 'w') as f:
+    # indent=2 is not needed but makes the file human-readable 
+    # if the data is nested
+        json.dump(values, f, indent=2)
+
+with open("SavedToDos.json", 'r') as f:
+    todos = json.load(f)
+
+    
+
+
+
+
+for _ in range(2):
     newitem = input("Was möchtest du hinzufügen?: ")
     if newitem in todos:
 
         todos.remove(newitem)
+        saveList(todos)
     
     else:
         todos.append(newitem)
+        saveList(todos)
 
     print("Das sind die Elemente:")
     for todo in todos:
         print(f"- {todo}")
 
-print("Ente Gelente")
+print("")
+print("Ende Gelände")
+
